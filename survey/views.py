a1d3c4502a7cc4f5b7ce0d0e3400a8e7b5f9754b
dylanPMurphy <dylanmurphydba@gmail.com>
2020-09-23 20:12:56 -0700

    Submission working

--- survey/views.py
+++ survey/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 
 # Create your views here.
 def index(request):
         return render(request, 'index.html')
+
+def submit(request):
+        context={
+                'firstName':request.POST['first_name'],
+                'lastName':request.POST['last_name'],
+                'birthday':request.POST['Birthday'],
+                'gender':request.POST['gender'],
+                'fav':request.POST['Favorite']
+        }
+        return render(request,'confirm.html', context)
